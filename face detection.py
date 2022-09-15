6500131391f76c204cec64a8033ff76be637311e
Mohd Nazim <76659303+mohdnazimali@users.noreply.github.com>
2022-09-15 17:33:38 +0530

    Update face detection.py

--- face detection.py
+++ face detection.py
@@ -1,13 +1,15 @@
 import cv2
+#this path is my local disk path image 
+image = cv2.imread("E:\\Data\\image\\[085657].jpg")
 
-imeg = cv2.imread("E:\\Data\\image\\[085657].jpg")
-gry = cv2.cvtColor(imeg, cv2.COLOR_BGR2GRAY)
+gry = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#this data det also in my local disk path 
 face = cv2.CascadeClassifier("E:\\Download\\haarcascade_frontalface_default.xml")
 faces = face.detectMultiScale(gry, 19, 2)
 for (x, y, w, h) in faces:
-    imeg = cv2.rectangle(imeg, (x, y), (x + w, y + h), (127, 0, 205), 8)
+    image = cv2.rectangle(image, (x, y), (x + w, y + h), (127, 0, 205), 8)
 
-imeg = cv2.resize(imeg, (900, 600))
-imeg = cv2.imshow("Nazim", imeg)
+image = cv2.resize(image, (900, 600))
+image = cv2.imshow("Nazim", image)
 cv2.waitKey()
 cv2.destroyAllWindows()
